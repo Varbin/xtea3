1ea47d9e905dd95fe04b2eeb043c5dd93ac6722b
Simon Biewald <simon.biewald@hotmail.de>
2018-05-16 20:39:13 +0200

    use counter from xtea

--- xtea3.py
+++ xtea3.py
@@ -45,61 +45,10 @@ import array
 from xtea import MODE_ECB, MODE_CBC, MODE_CFB, MODE_CTR, MODE_OFB, MODE_PGP
 from xtea import new, XTEACipher
 from xtea import key_size, block_size
+from xtea.counter import Counter
 
 __all__ = ["MODE_PGP", "MODE_OFB", "MODE_CTR", "MODE_CFB", "MODE_CBC", "MODE_ECB",
            "new", "XTEACipher", "Counter", "key_size", "block_size"]
-
-
-class Counter:
-    """Small counter for CTR mode, based on arrays
-
-    Example:
-    
-        >>> from xtea3 import Counter
-        >>> nonce = b"$2dUI84e" # This should be random
-        >>> c = Counter(nonce)
-        >>> c()
-        b'%2dUI84e'
-        >>> c()
-        b'&2dUI84e'
-        >>> c()
-        b"'2dUI84e"
-        >>> c.reset()
-        >>> c()
-        b'%2dUI84e'
-    """
-    
-    def __init__(self, nonce):
-        """Constructor for a counter which is suitable for CTR mode.
-
-        Args:
-            nonce (bytes): The start value;
-                it MUST be unique to be secure.
-                The secrets module or os.urandom(n) are solid
-                choices for generating random bytes.
-
-        """
-        self.__nonce = nonce
-        self.__current = array.array("B", self.__nonce)
-
-    def __call__(self):
-        """Increase the counter by one.
-
-        Returns:
-            bytes
-        """
-        for i in range(len(self.__current)):
-            try:
-                self.__current[i] += 1
-                break
-            except IndexError:
-                self.__current[i] = 0
-        return self.__current.tostring()
-    
-    def reset(self):
-        """Reset the counter to the nonce."""
-        self.__current = array.array("B", self.__nonce)
-
 
 
 if __name__ == "__main__":
